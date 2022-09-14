d0b678500219e04ec571fecefafad42334e5a009
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-09-14 14:44:12 +0300

    add-profilepage-js

---
+++ front-end/script/script.js
@@ -0,0 +1,17 @@
+console.log("hahah");
+
+const editBtnEL = document.querySelector(".edit-profile-btn")
+const popupEl = document.querySelector(".popup")
+const backdropEl = document.querySelector(".backdrop")
+const closeEL = document.getElementById("close")
+
+close = () => {
+    popupEl.classList.toggle("none")
+    backdropEl.classList.toggle("none")
+}
+
+editBtnEL.addEventListener('click', close)
+
+backdropEl.addEventListener('click', close)
+
+closeEL.addEventListener('click', close)
