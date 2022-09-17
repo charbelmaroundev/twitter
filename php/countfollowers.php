4868834a188d8df081b3e850dbdfa0582431ab1a
Mazennaja123 <63180234+Mazennaja123@users.noreply.github.com>
2022-09-18 02:50:41 +0300

    Adding Followers count api

--- php/countfollowers.php
+++ php/countfollowers.php
@@ -13,7 +13,7 @@ $follower_id = $_POST["follower_id"];
 
 
 $query = $mysqli->prepare("SELECT COUNT(following_id) FROM user_follow_user WHERE following_id = ? LIMIT 1");
-$query->bind_param("s", $following_id);
+$query->bind_param("s", $follower_id);
 $query->execute();
 
 
