7efa8d1e9daac82586a3ba8d00c054666035b83e
Mazennaja123 <63180234+Mazennaja123@users.noreply.github.com>
2022-09-17 22:58:33 +0300

    Added add follower feature

---
+++ php/add_new_follower.php
@@ -0,0 +1,25 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+
+include("connection.php");
+
+
+$follower_id =$_POST["follower_id"];
+$following_id = $_POST["following_id"];
+
+
+$query = $mysqli->prepare("INSERT INTO followers(follower_id,following_id) VALUE (?,?)");
+$query->bind_param("sss", $follower_id, $following_id);
+$query->execute();
+
+
+
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response);
+
+?>
