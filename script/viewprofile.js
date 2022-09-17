8cc8d846c1022a95c7c4eb4220829aa3807d911a
Mazennaja123 <63180234+Mazennaja123@users.noreply.github.com>
2022-09-18 01:57:50 +0300

    Adding follow feature testing phase 1

--- script/viewprofile.js
+++ script/viewprofile.js
@@ -4,20 +4,40 @@ const profileUsernameUserEl = document.querySelector('.profile-username-user');
 const imageProfileImgEl = document.querySelector('.image-profile-img');
 const accountImg1El = document.querySelector('.account-img');
 const followrequest =document.querySelector('follow-btn');
-fetch("http://localhost/twitter/fetchdata_id.php", {
-    method: 'POST',
-    body: new URLSearchParams({
-        id: localStorage.getItem("destination")
-    })
-})
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        accountName1El.innerHTML = `${data[0]['firstname']} ${data[0]['lastname']}`
-        profileUsernameNameEl.innerHTML = `${data[0]['firstname']} ${data[0]['lastname']}`
-        profileUsernameUserEl.innerHTML = `@${data[0]['username']}`
-        imageProfileImgEl.src = data[0]['image']
-    })
+
+
+
+
+
+//
+// fetch("http://localhost/twitter/fetchdata_id.php", {
+//     method: 'POST',
+//     body: new URLSearchParams({
+//         id: localStorage.getItem("destination")
+//     })
+// })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//         accountName1El.innerHTML = `${data[0]['firstname']} ${data[0]['lastname']}`
+//         profileUsernameNameEl.innerHTML = `${data[0]['firstname']} ${data[0]['lastname']}`
+//         profileUsernameUserEl.innerHTML = `@${data[0]['username']}`
+//         imageProfileImgEl.src = data[0]['image']
+//     })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -34,7 +54,7 @@ fetch("http://localhost/twitter/fetchdata_id.php", {
     })
         .then(response => response.json())
         .then(data => {
-          
+
             console.log(data);
 
         })
