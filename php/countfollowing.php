a279e6977be568fcc5a16fb1326401815b68ccf5
Mazennaja123 <63180234+Mazennaja123@users.noreply.github.com>
2022-09-18 00:46:24 +0300

    Adding count following feature

---
+++ php/countfollowing.php
@@ -0,0 +1,24 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+
+include("connection.php");
+
+
+
+$follower_id = $_POST["follower_id"];
+
+
+
+$query = $mysqli->prepare("SELECT COUNT(follower_id) FROM user_follow_user WHERE follower_id = ? LIMIT 1");
+$query->bind_param("s", $follower_id);
+$query->execute();
+
+
+$response = [];
+$response["success"] = true;
+echo json_encode($response);
+
+?>
