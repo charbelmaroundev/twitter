bca0e43232c6ebd027066a7c391471680457466f
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-09-18 09:35:34 +0300

    add-profilepage-js

--- front-end/script/homepage.js
+++ front-end/script/homepage.js
@@ -1,10 +1,8 @@
 const accountImgEl = document.querySelectorAll('#account-img')
 const accountDetailsUsernameEl = document.querySelector('.account-details-username')
 const accountDetailsName = document.querySelector('.account-details-name')
-const accountDetailsUsername = document.querySelector('.account-details-username')
 const searchEl = document.getElementById('search');
 const searchResultsEl = document.querySelector('.search-results');
-const accountNameEl = document.querySelector('.account-name');
 
 let url = "http://localhost/twitter/fetchdata_id.php";
 let parameters = {
@@ -18,7 +16,7 @@ fetch(url, parameters)
     .then(data => {
         // console.log(data)
         accountDetailsName.innerHTML = `${data[0]['firstname']} ${data[0]['lastname']}`
-        accountDetailsUsername.innerHTML = `@${data[0]['username']}`
+        accountDetailsUsernameEl.innerHTML = `@${data[0]['username']}`
         accountImgEl.forEach((img) => {
             img.src = data[0]['image']
         })
---
+++ front-end/script/profilepage.js
@@ -0,0 +1,71 @@
+const editBtnEL = document.querySelector(".edit-profile-btn")
+const popupEl = document.querySelector(".popup")
+const backdropEl = document.querySelector(".backdrop")
+const closeEL = document.getElementById("close")
+const editImgEl = document.getElementById('file-input')
+const editSaveEl = document.getElementById('edit-save')
+const editNameEl = document.getElementById('edit-name')
+const accountNameEl = document.querySelector('.account-name')
+const profileUsernameNameEl = document.querySelector('.profile-username-name');
+const imageProfileImgEl = document.querySelector('.image-profile-img');
+const accountImg = document.querySelector('.account-img')
+const profileUsernameUserEl = document.querySelector('.profile-username-user');
+const popupimgImgEl = document.querySelector('.popupimg-img');
+
+updateProfile = () => {
+    fetch("http://localhost/twitter/fetchdata_id.php", parameters = {
+        method: 'POST',
+        body: new URLSearchParams({
+            id: localStorage.getItem("id")
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            accountNameEl.innerHTML = `${data[0]['firstname']} ${data[0]['lastname']}`;
+            profileUsernameNameEl.innerHTML = `${data[0]['firstname']} ${data[0]['lastname']}`;
+            profileUsernameUserEl.innerHTML = `@${data[0]['username']}`;
+            imageProfileImgEl.src = data[0]['image'];
+            accountImg.src = data[0]['image'];
+            popupimgImgEl.src = data[0]['image'];
+            editNameEl.value = `${data[0]['firstname']} ${data[0]['lastname']}`;
+        })
+}
+updateProfile()
+
+close = () => {
+    popupEl.classList.toggle("none")
+    backdropEl.classList.toggle("none")
+}
+
+editSaveEl.addEventListener("click", () => {
+    const readFile = editImgEl => {
+        const FR = new FileReader();
+        FR.addEventListener("load", (evt) => {
+            srcData = evt.target.result
+            console.log(srcData);
+            const [first, last] = editNameEl.value.split(' ');
+            fetch('http://localhost/twitter/editprofile.php', {
+                method: 'POST',
+                body: new URLSearchParams({
+                    id: localStorage.getItem("id"),
+                    firstname: first,
+                    lastname: last,
+                    image: srcData
+                })
+            })
+                .then(response => response.json())
+                .then(data => console.log(data))
+        });
+        FR.readAsDataURL(editImgEl.files[0]);
+
+    }
+    readFile(editImgEl);
+    updateProfile();
+    close();
+})
+
+editBtnEL.addEventListener('click', close)
+backdropEl.addEventListener('click', close)
+closeEL.addEventListener('click', close)
+
+
